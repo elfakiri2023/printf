f3f8f6312a0beafa8a7d34c790d5c3a81da5c231
Badr-Annabi <badrannabi@gmail.com>
2023-09-19 06:55:04 -0700

    Fixing some bugs

--- get_flags.c
+++ get_flags.c
@@ -1,30 +1,28 @@
 #include "main.h"
 
 /**
- * get_the_flags - Calculates active flags
+ * get_the_flags - Calculates active flags so it can handle them.
  * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
+ * @i: take a parameter as an input
  * Return: Flags:
  */
 int get_the_flags(const char *format, int *i)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
-	int j, curr_i;
+	int curr_i, a;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
+		for (a = 0; FLAGS_CH[a] != '\0'; a++)
+			if (format[curr_a] == FLAGS_CH[a])
 			{
-				flags |= FLAGS_ARR[j];
+				flags |= FLAGS_ARR[a];
 				break;
 			}
 
-		if (FLAGS_CH[j] == 0)
+		if (FLAGS_CH[a] == 0)
 			break;
 	}
 
