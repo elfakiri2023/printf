9cf817ff320d74cad8477052af6957e664c332d9
Badr-Annabi <badrannabi@gmail.com>
2023-09-18 17:16:15 -0700

    Fixing precision

--- get_precision.c
+++ get_precision.c
@@ -10,7 +10,7 @@
  */
 int get_the_precision(const char *format, int *i, va_list list)
 {
-	int curr_i = *i + 1;
+	int curr_i = *i;
 	int precision = -1;
 
 	if (format[curr_i] != '.')
@@ -18,24 +18,23 @@ int get_the_precision(const char *format, int *i, va_list list)
 
 	precision = 0;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	curr_i++;
+
+	if (format[curr_i] == '*')
 	{
-		if (check_is_digit(format[curr_i]))
+		curr_i++;
+		precision = va_arg(list, int);
+	}
+	else
+	{
+		while (is_digit(format[curr_i]))
 		{
-			precision *= 10;
-			precision += format[curr_i] - '0';
-		}
-		else if (format[curr_i] == '*')
-		{
+			precision = precision * 10 + (format[curr_i] - '0');
 			curr_i++;
-			precision = va_arg(list, int);
-			break;
 		}
-		else
-			break;
 	}
 
-	*i = curr_i - 1;
+	*i = curr_i;
 
 	return (precision);
 }
